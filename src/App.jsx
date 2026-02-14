d6f69a37a3bd93f961b7479faf73f8282f6bdbc3
Mohamed0120 <hamoudy0120@gmail.com>
2026-02-14 22:37:41 +0200

    add content to app & delete assets folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 export default function App() {
 
   return (
-    <></>
+    <>
+    <h1>hello world</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere dolores saepe nam laborum autem fugiat ex, vel amet excepturi nesciunt. Sint natus earum nostrum commodi quas ea molestias labore harum Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quibusdam beatae expedita, reprehenderit quaerat quam voluptas animi minima ad officiis non consequatur consequuntur magnam labore vel laudantium ullam adipisci sunt iure!
+    </p>
+    </>
   )
 }
 
